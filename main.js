7f33229ac3e34ea468a59cb57770cdd56ea6294e
Jennicoleperez <77292561+Jennicoleperez@users.noreply.github.com>
2023-02-05 17:25:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*BARRA DE NAVEGACIÓN */
 const menu = document.querySelector('.menu')
 const links = document.querySelector('.links')
 
@@ -6,13 +7,14 @@ menu.addEventListener('click', () => {
     links.classList.menu('activar')
 })
 
+/*SLIDER */
 
 let index = 0,
   sliders,
   timer,
   next,
   prev;
-  
+
 document.addEventListener('DOMContentLoaded', function() {
 
   slides = document.querySelectorAll(".mySlides");
@@ -56,10 +58,7 @@ function showSlides(n) {
 
 
 
-
-
-
-
+ /*CONTÁCTANOS */
 
 var inputs= document.getElementsByClassName('formulario_input');
 for (var i=0; i<inputs.length; i++){
@@ -113,3 +112,75 @@ for (var i=0; i<inputs.length; i++){
         email.value = "";
         mensaje.value = ""; 
     });
+
+/*HABILIDADES */
+
+    data = {
+        task1: {
+          progress: 0.5,
+          name: "Javascript"
+        },
+        task2: {
+          progress: 0.5,
+          name: "Python"
+        },
+        task3: {
+          progress: 0.8,
+          name: "HTML"
+        },
+        task4: {
+          progress: 0.7,
+          name: "CSS"
+        },
+        task5: {
+          progress: null,
+          name: "GitHub"
+        }
+
+
+      };
+      
+      
+      wraper_progressbar = document.querySelector(".wraper-progressbar");
+      wp = wraper_progressbar;
+      
+      for (task in data) {
+        elem_label = document.createElement("label");
+      
+        elem_span = document.createElement("span");
+      
+        elem_span_text = document.createTextNode(data[task].name);
+      
+        elem_div_task = document.createElement("div");
+        elem_div_task.setAttribute("id", "task");
+        elem_div_task.classList.add("task");
+      
+        elem_div_progress = document.createElement("div");
+        elem_div_progress.classList.add("progress");
+      
+
+        progress = data[task].progress;
+        position = 272 * progress;
+        elem_div_progress.style.left = position + "px";
+        if (progress < 0.25) {
+          elem_div_progress.classList.add("progress-red");
+        } else if (progress < 0.75) {
+          elem_div_progress.classList.add("progress-orange");
+        } else {
+          elem_div_progress.classList.add("progress-green");
+        }
+      
+          elem_div_tooltip = document.createElement("div");
+        elem_div_tooltip.classList.add("tooltip");
+        elem_div_tooltip_text = document.createTextNode(progress * 100 + ' %');
+        
+
+        elem_span.appendChild(elem_span_text);
+        elem_label.appendChild(elem_span);
+        elem_div_tooltip.appendChild(elem_div_tooltip_text);
+        elem_div_progress.appendChild(elem_div_tooltip);
+        elem_div_task.appendChild(elem_div_progress);
+        elem_label.appendChild(elem_div_task);
+      
+        wp.appendChild(elem_label);
+      }
