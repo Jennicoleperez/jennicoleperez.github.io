aeb6c5b1cbcea177c29986f0a1617cbac8c0a179
Jennicoleperez <77292561+Jennicoleperez@users.noreply.github.com>
2023-02-05 18:54:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.menu')
 const links = document.querySelector('.links')
 
 menu.addEventListener('click', () => {
-    menu.classList.menu('rotate')
+    menu.classList.menu('rotate'),
     links.classList.menu('activar')
 });
 
