e4095beaa504ec2e5ea7249f1dedc775f85eff68
Jennicoleperez <77292561+Jennicoleperez@users.noreply.github.com>
2023-02-05 10:11:23 -0400

    Delete Microproyecto1 directory

--- Microproyecto1/main.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('.menu')
-const links = document.querySelector('.links')
-
-menu.addEventListener('click', () => {
-    menu.classList.menu('rotate')
-    links.classList.menu('activar')
-})
-
