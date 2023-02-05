81ae0bfd34bb408c7fbaa23651978e6ecf4ebc0c
Jennicoleperez <77292561+Jennicoleperez@users.noreply.github.com>
2023-02-05 12:55:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ menu.addEventListener('click', () => {
     links.classList.menu('activar')
 })
 
+var inputs= document.getElementsByClassName('formulario_input');
+for (var i=0; i<inputs.length; i++){
+    inputs[i].addEventListener('keyup', function(){
+        if(this.value.length >=1 ) {
+            this.nextElementSibling.classList.add('fijar');
+        }else{
+            this.nextElementSibling.classList.remove('fijar');
+        }
+
+    });
+
+}
+
